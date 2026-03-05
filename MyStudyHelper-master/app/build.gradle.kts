5a8020cdfd8a7171c6ec86513fccc844073471d5
juca <theepapirrin09@gmail.com>
2026-03-04 21:36:50 -0600

    Implementación base MVVM - Lógica de estrés funcional

--- MyStudyHelper-master/app/build.gradle.kts
+++ MyStudyHelper-master/app/build.gradle.kts
@@ -5,14 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mystudyhelper"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mystudyhelper"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -41,8 +39,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
